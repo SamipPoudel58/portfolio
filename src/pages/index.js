3cde659e3661f1fb1724ae639aeddf6ba63b6a22
Samip Poudel <sawmeep58@gmail.com>
2021-05-16 12:14:21 +0545

    setup necessary packages and sass files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import "../styles/index.scss"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Red</h1>
+      Hello world!
+    </div>
+  )
 }
