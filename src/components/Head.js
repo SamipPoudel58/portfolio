7a9b62242c405f0e41e94820e7a78b69019a4699
Samip Poudel <sawmeep58@gmail.com>
2021-07-01 15:52:48 +0545

    feat: add analytics and opengraph

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,6 +6,7 @@ const Head = ({ title }) => {
     <Helmet>
       <html lang="en" />
       <title>{`${title} | Samip Poudel`}</title>
+
       <meta
         name="description"
         content="Fullstack Web Developer, Javascript | React | Node.js | MongoDB"
@@ -14,6 +15,24 @@ const Head = ({ title }) => {
         name="keyword"
         content="samip, samip poudel, portfolio, javascript, developer, blog"
       ></meta>
+
+      <meta
+        property="og:title"
+        content="Colby Fayock - Software Engineer &amp; Freelancer"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/open_graph.png" />
+      <meta
+        property="og:description"
+        content="Fullstack Web Developer, Javascript | React | Node.js | MongoDB"
+      />
+      <meta property="og:url" content="https://www.samippoudel.com.np" />
+
+      <script
+        defer
+        data-domain="samippoudel.com.np"
+        src="https://plausible.io/js/plausible.js"
+      ></script>
     </Helmet>
   )
 }
