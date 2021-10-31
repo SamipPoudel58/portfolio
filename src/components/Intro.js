1b84084d729891783147675acdcbb939baeeb576
Samip Poudel <sawmeep58@gmail.com>
2021-10-31 22:47:41 +0545

    style: haloween special easter egg 🎃

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,10 +5,18 @@ const Intro = () => {
     <section className="intro__wrapper">
       <div className="intro">
         <div className="intro__content">
-          <p className="intro__hello">Hello, I'm Samip</p>
-          <h1 className="intro__info">I build Websites</h1>
+          <p className="intro__hello">Happy Halloween, I'm a spooky</p>
+          <h1 className="intro__info">
+            Full-Stack Developer{" "}
+            <span role="img" aria-label="ghost emoji">
+              👻
+            </span>
+          </h1>
           <a className="intro__cta" href="/#contact">
-            Contact Me
+            Contact Me{" "}
+            <span role="img" aria-label="evil emoji">
+              😈
+            </span>
           </a>
         </div>
       </div>
