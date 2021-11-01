6d49f815ba0c3d902aea54c31e1300e50138a007
Samip Poudel <sawmeep58@gmail.com>
2021-11-01 21:09:55 +0545

    style: restyle intro section

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,18 +5,23 @@ const Intro = () => {
     <section className="intro__wrapper">
       <div className="intro">
         <div className="intro__content">
-          <p className="intro__hello">Happy Halloween, I'm a spooky</p>
-          <h1 className="intro__info">
-            Full-Stack Developer{" "}
-            <span role="img" aria-label="ghost emoji">
-              👻
-            </span>
-          </h1>
+          <img
+            className="intro__sun"
+            src="/retro-sun.png"
+            alt="retro sun background"
+          />
+          <h1 className="intro__info text-center">Developer | Designer</h1>
+
+          {/* <p className="intro__hello">Hello, I'm a</p>
+          <h1 className="intro__info">Full-Stack Developer</h1>
+          <p className="intro__subinfo">
+            who loves designing and building :&#41;
+          </p>
+          */}
+        </div>
+        <div className="intro__ctaHolder">
           <a className="intro__cta" href="/#contact">
-            Contact Me{" "}
-            <span role="img" aria-label="evil emoji">
-              😈
-            </span>
+            Contact Me
           </a>
         </div>
       </div>
