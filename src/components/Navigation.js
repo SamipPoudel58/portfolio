1bf102740bf181ffcba0b24735119c7e73c9b7cd
Samip Poudel <sawmeep58@gmail.com>
2022-01-02 23:30:55 +0545

    fix: dark mode toggle

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,17 +2,19 @@ import { Link } from "gatsby"
 import React, { useState, useEffect } from "react"
 
 const Navigation = () => {
-  // const [navbar, setNavbar] = useState(active)
+  const [darkMode, setDarkMode] = useState(false)
   const [showMobileNav, setMobileNav] = useState(false)
 
   useEffect(() => {
-    if (localStorage.getItem("darkMode")) {
+    if (JSON.parse(localStorage.getItem("darkMode")) === true) {
+      setDarkMode(true)
       document.body.classList.add("dark")
     }
-  })
+  }, [])
 
   const themeToggle = () => {
-    const darkMode = localStorage.getItem("darkMode")
+    // const darkMode = localStorage.getItem("darkMode")
+    setDarkMode(prevValue => !prevValue)
     localStorage.setItem("darkMode", !darkMode)
 
     document.body.classList.toggle("dark")
@@ -120,14 +122,29 @@ const Navigation = () => {
         </nav>
 
         <button onClick={themeToggle} className="navigation__theme">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-          </svg>
+          {darkMode ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+            </svg>
+          )}
         </button>
       </div>
       <button
